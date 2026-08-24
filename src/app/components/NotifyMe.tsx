a003a55893f5c4d84cf245cb37a7cb2fe17fa0f0
Anshu Sony <anshu.sony@agilonhealth.com>
2026-08-24 22:59:17 +0530

    bug fixed

--- src/app/components/NotifyMe.tsx
+++ src/app/components/NotifyMe.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NOTIFY_ME_FORM_ENDPOINT, RECAPTCHA_SITE_KEY } from '@/lib/config';
 import { trackEvent } from '@/lib/analytics';
 
